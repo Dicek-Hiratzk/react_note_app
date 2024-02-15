b21c0cbc5c46e7c47c1f1845c45a91bcc630c966
Dicek-Hiratzk <diezoc24@gmail.com>
2024-02-15 14:25:46 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
+import uuid from "react-uuid";
+
+function App() {
+  const [notes, setNotes] = useState(
+    // ローカルストレージにデータがあれば返す
+    // JSON形式からオブジェクトに戻す必要あり
+    JSON.parse(localStorage.getItem("notes")) || []
+    ); 
+  const [activeNote, setActiveNote] = useState(false);
+
+  // ローカルストレージ保存用
+  useEffect(() => {
+    // ローカルストレージにノートを保存する
+    // JSON形式じゃないと入力した文字列で保存されない
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]); // 第二引数のnotesの中身が変わる度に第一引数のCB関数が発火する
+
+  // リロード時一番上のノートを開いた状態にする
+  // ifはノートがないときエラー吐かないようにするため
+  useEffect(() => {
+    if (notes.length !== 0) setActiveNote(notes[0].id);
+  }, []);
+
+  // ノート追加ロジック
+  const onAddNote = () => {
+    console.log("新しくノートが追加されました")
+    const newNote = {
+      id: uuid(), // ランダムなidを指定
+      title: "",
+      content: "",
+      modDate: Date.now(), // いつ修正したのか
+    };
+    // ...notesは以前のノート(newNoteで新しいノートを追加するため)
+    setNotes([...notes, newNote]); // 更新用
+    console.log(notes);
+  };
+
+  // ノート削除ロジック
+  const onDeleteNote = (id) => {
+    // true(この場合該当idではない)を残すためfilter関数
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes); // 更新用
+  };
+
+  // プレビュー用のオブジェクトを取得するためのロジック
+  const getActiveNote = () => {
+    // find()でノートを見ていき返す
+    return notes.find((note) => note.id === activeNote)
+  };
+
+  // ノートの更新
+  const onUpdateNote = (updatedNote) => {
+    // 修正された新しいノートの配列を返す
+    const updatedNoteArray = notes.map((note) => {
+      if(note.id === updatedNote.id) {
+        // console.log(updatedNote);
+        return updatedNote; // 編集中のノート
+      } else {
+        return note; // 残りのノート
+      }
+    });
+    // console.log(updatedNoteArray);
+    setNotes(updatedNoteArray);
+  };
+
+  return (
+    <div className="App">
+      <Sidebar 
+        onAddNote={onAddNote} 
+        notes={notes} 
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main 
+        activeNote={getActiveNote()} 
+        onUpdateNote={onUpdateNote}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Main.css";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+const Main = ({ activeNote, onUpdateNote }) => {
+  
+  // 編集内容更新ロジック
+  const onEditNote = (key, value) => {
+    onUpdateNote({
+      // id: activeNote.id, // 片方の編集のみ表示なのでNG
+      ...activeNote, // タイトルとコンテンツ各々表示編集するため
+      [key]: value, // [key]は動的キー
+      modDate: Date.now(),
+    })
+    // console.log([key]);
+  };
+  // key: title, constent: 第一引数
+  // value: e.target.value: 第二引数
+
+  // activeNoteが初期値がfalseのため表示されるよう設定
+  if(!activeNote) {
+    return <div className="no-active-note">ノートが選択されていません</div>
+  }
+
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input 
+          id="title"
+          type="text" 
+          placeholder="タイトルを記入" 
+          value={activeNote.title} 
+          onChange={(e) => onEditNote("title", e.target.value)}
+        />
+        <textarea 
+          id="content" 
+          placeholder="本文を記入" 
+          value={activeNote.content}
+          onChange={(e) => onEditNote("content", e.target.value)}
+        ></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">{activeNote.title}</h1>
+        <Markdown className="markdown-preview" remarkPlugins={remarkGfm}>
+          {activeNote.content}
+        </Markdown>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
